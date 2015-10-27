33b6ece90caa9e232b7dba9d4c93dcf3c357e5ea
xiang <bylixiang@gmail.com>
2015-10-26 21:25:25 -0700

    fix bugs:     app/forms.py     app/view.py
    add virtualenvsetup.sh

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import os, sys
 """ start the server """
 
-sys.path.append(os.path.dirname(__file__))
+#sys.path.append(os.path.dirname(__file__))
 
 # import app variable from our app package
 from app import app
