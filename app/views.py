0f713c8923e548bc59440804e64299a521280997
Xiang <xiang@silvustechnologies.com>
2015-10-27 17:24:29 -0700

    Feat: post field in index page

--- app/views.py
+++ app/views.py
@@ -1,31 +1,28 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm, EditForm
-from .models import User
+from .forms import LoginForm, EditForm, PostForm
+from .models import User, Post
 from oauth import OAuthSignIn
 from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required  # add login_required
 def index():
-    user = g.user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index')) # avoid re-submiting
+    posts = g.user.followed_posts().all()
     return render_template('index.html',
                             title='Home',
-                            user=user,
-                            posts=posts)
+                            posts=posts,
+                            form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
