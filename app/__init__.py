5a92b2c54773a5f64445afd4e8b29d24e7a20bdc
Xiang <xiang@silvustechnologies.com>
2015-10-29 10:25:20 -0700

    Doc: add some comments and notes

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,19 @@ from flask.ext.login import LoginManager, UserMixin
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)  # initialize database
+# This tells jinja2 to expost our class as a global variable to all templates
+app.jinja_env.globals['momentjs'] = momentjs
 
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'oauth'
-# the Flask-OPenID requeires a path to a temp folder where files can be stored
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# the Flask-OpenID requeires a path to a temp folder where files can be stored
+# oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 mail = Mail(app)
 
