cc296912194348e213e08852b8f2dd6942a255a9
Xiang <xiang@silvustechnologies.com>
2015-10-30 13:25:48 -0700

    Heroku: change logging for heroku

--- config.py
+++ config.py
@@ -27,7 +27,10 @@ OAUTH_CREDENTIALS = {
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # required by `FLASK-SQLAlchemy`
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 # where we store the SQLALchemy-migrate data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
